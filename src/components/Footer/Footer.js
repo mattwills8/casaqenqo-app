aae25234e4eb5632d415a86c759941539647010a
mattwills8 <matt_wills8@hotmail.co.uk>
2018-05-18 15:19:37 -0500

    add footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,34 @@
 // @flow
 import * as React from 'react'
+import { text } from '../../lib/cq_language'
+import style from './Footer.scss'
 
 type Props = {}
 
 class Footer extends React.Component<Props> {
+  renderFooterMenuItems () {
+    return (
+      <div className={style.menuItems}>
+        <div className={style.menuItem}>
+          <h5>{text('News')}</h5>
+        </div>
+        <div className={style.menuItem}>
+          <h5>{text('Casa')}</h5>
+        </div>
+        <div className={style.menuItem}>
+          <h5>{text('Services')}</h5>
+        </div>
+        <div className={style.menuItem}>
+          <h5>{text('Project')}</h5>
+        </div>
+      </div>
+    )
+  }
+
   render () {
-    return <div>{'Footer'}</div>
+    return (
+      <footer className={style.footer}>{this.renderFooterMenuItems()}</footer>
+    )
   }
 }
 
