7265a7539d99fc98c291ebac222d91a3ab1cc010
mattwills8 <matt_wills8@hotmail.co.uk>
2018-05-18 11:04:35 -0500

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,12 @@
+// @flow
+import * as React from 'react'
+
+type Props = {}
+
+class App extends React.Component<Props> {
+  render () {
+    return <div>{'Casa Qenqo'}</div>
+  }
+}
+
+export default App
