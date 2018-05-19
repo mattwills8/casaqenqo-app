9fdece28c46f7abeb7b8e724d7314aae36273cd5
mattwills8 <matt_wills8@hotmail.co.uk>
2018-05-18 21:59:08 -0500

    add loading spinner

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,11 +1,16 @@
 // @flow
 import * as React from 'react'
+import style from './Loading.scss'
 
 type Props = {}
 
 class Loading extends React.Component<Props> {
   render () {
-    return <div style={{ height: '100vh' }} />
+    return (
+      <div style={{ height: '100vh', paddingTop: '40vh', textAlign: 'center' }}>
+        <div className={style.spinner} />
+      </div>
+    )
   }
 }
 
