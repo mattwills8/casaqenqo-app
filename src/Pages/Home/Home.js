ba9c7ea0747fbdc7cbb915a23903e4698188dcc6
mattwills8 <matt_wills8@hotmail.co.uk>
2018-05-25 23:20:50 -0500

    update photos

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -2,7 +2,12 @@
 import * as React from 'react'
 import Hero from '../../components/Hero/Hero'
 import PageSection from '../../components/PageSection/PageSection'
-import { green, lightgray, white } from '../../scss/partials/_variables.scss'
+import {
+  green,
+  lightgray,
+  white,
+  silver
+} from '../../scss/partials/_variables.scss'
 import Underline from '../../components/Underline/Underline'
 import style from './Home.scss'
 
@@ -16,14 +21,14 @@ class Home extends React.Component<Props> {
           title={"Casa Q'enqo"}
           copy={'Help us help homeless children in Cusco'}
           imgSrc={
-            'http://localhost/casaqenqo/wp-content/uploads/2018/05/IMG_1316.jpg'
+            'http://localhost/casaqenqo/wp-content/uploads/2018/05/30602238_1778785328826315_4105638377313796096_n.jpg'
           }
           overlay={green}
         />
         <PageSection
           alignment={'left'}
           imgSrc={
-            'http://localhost/casaqenqo/wp-content/uploads/2018/05/IMG_1316.jpg'
+            'http://localhost/casaqenqo/wp-content/uploads/2018/05/31729856_371738433309886_3474435251755810816_n-e1527308033517.jpg'
           }
           overlay={lightgray}
           contentWidth={'2'}
@@ -31,6 +36,25 @@ class Home extends React.Component<Props> {
             return (
               <div className={style.section1}>
                 <h2>The Project</h2>
+                <Underline style={{ width: '65%' }} align={'left'} />
+                <div>
+                  {'Some explanation hre of whats happening Lorem Ipsum Dolor'}
+                </div>
+              </div>
+            )
+          }}
+        />
+        <PageSection
+          alignment={'right'}
+          overlay={lightgray}
+          contentWidth={'2'}
+          imgSrc={
+            'http://localhost/casaqenqo/wp-content/uploads/2018/05/DSC_0721-2-1024x683.jpg'
+          }
+          renderContent={() => {
+            return (
+              <div className={style.section1}>
+                <h2>The Casa</h2>
                 <Underline style={{ width: '65%' }} align={'left'} />
                 <div>
                   {'Some explanation hre of whats happening Lorem Ipsum Dolor'}
@@ -55,16 +79,16 @@ class Home extends React.Component<Props> {
           }}
         />
         <PageSection
-          alignment={'right'}
-          overlay={lightgray}
+          alignment={'left'}
+          overlay={silver}
           contentWidth={'2'}
           imgSrc={
-            'http://localhost/casaqenqo/wp-content/uploads/2018/05/IMG_1316.jpg'
+            'http://localhost/casaqenqo/wp-content/uploads/2018/05/DSC_0194-1024x683.jpg'
           }
           renderContent={() => {
             return (
               <div className={style.section1}>
-                <h2>The Casa</h2>
+                <h2>About Luis</h2>
                 <Underline style={{ width: '65%' }} align={'left'} />
                 <div>
                   {'Some explanation hre of whats happening Lorem Ipsum Dolor'}
